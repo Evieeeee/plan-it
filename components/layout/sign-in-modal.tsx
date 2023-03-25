f5d523167c60035e192b2ae27879320851355148
Evieeeee <australian.starlight@gmail.com>
2023-03-25 15:05:54 +1100

    Actually have a next JS app, a working database and components that dont actually work :D
    Signed-off-by: Evieeeee <australian.starlight@gmail.com>

---
+++ components/layout/sign-in-modal.tsx
@@ -0,0 +1,99 @@
+import Modal from "@/components/shared/modal";
+import { signIn } from "next-auth/react";
+import {
+  useState,
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useMemo,
+} from "react";
+import { LoadingDots, Google, Discord } from "@/components/shared/icons";
+import Image from "next/image";
+import useSWR from 'swr';
+
+const SignInModal = ({
+  showSignInModal,
+  setShowSignInModal,
+}: {
+  showSignInModal: boolean;
+  setShowSignInModal: Dispatch<SetStateAction<boolean>>;
+}) => {
+  const [signInClicked, setSignInClicked] = useState(false);
+
+  return (
+    <Modal showModal={showSignInModal} setShowModal={setShowSignInModal}>
+      <div className="w-full overflow-hidden shadow-xl md:max-w-md md:rounded-2xl md:border md:border-gray-200">
+        <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center md:px-16">
+          <a href="https://precedent.dev">
+            <Image
+              src="/logo.png"
+              alt="Logo"
+              className="h-10 w-10 rounded-full"
+              width={20}
+              height={20}
+            />
+          </a>
+          <h3 className="font-display text-2xl font-bold">Sign In</h3>
+          <p className="text-sm text-gray-500">
+            We will never expose your personal data to anyone else and will only store your email and profile picture, you can choose to delete these.
+			Only the Username that you select is exposed to any other person.
+					
+          </p>
+        </div>
+        <div className="flex flex-col space-y-4 bg-gray-50 px-4 py-8 md:px-16">
+          <button
+            disabled={signInClicked}
+            className={`${
+              signInClicked
+                ? "cursor-not-allowed border-gray-200 bg-gray-100"
+                : "border border-gray-200 bg-white text-black hover:bg-gray-50"
+            } flex h-10 w-full items-center justify-center space-x-3 rounded-md border text-sm shadow-sm transition-all duration-75 focus:outline-none`}
+            onClick={() => {
+              setSignInClicked(true);
+              signIn("discord");
+            }}
+          >
+            {signInClicked ? (
+              <LoadingDots color="#808080" />
+            ) : (
+              <>
+                <Discord className="h-5 w-5" />
+                <p>Sign In with Discord</p>
+              </>
+            )}
+          </button>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export function useSignInModal() {
+  const [showSignInModal, setShowSignInModal] = useState(false);
+
+  const SignInModalCallback = useCallback(() => {
+    return (
+      <SignInModal
+        showSignInModal={showSignInModal}
+        setShowSignInModal={setShowSignInModal}
+      />
+    );
+  }, [showSignInModal, setShowSignInModal]);
+
+  return useMemo(
+    () => ({ setShowSignInModal, SignInModal: SignInModalCallback }),
+    [setShowSignInModal, SignInModalCallback],
+  );
+}
+
+
+ 
+function Profile () {
+  const { data, error, isLoading } = useSWR('/api/user/123', fetcher)
+ 
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+ 
+  // render data
+  return <div>hello {data.name}!</div>
+}
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,24 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/lib/prisma";
+import GoogleProvider from "next-auth/providers/google";
+import DiscordProvider from "next-auth/providers/discord";
+
+
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+	  allowDangerousEmailAccountLinking: true
+    }),DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+	  allowDangerousEmailAccountLinking: true
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
